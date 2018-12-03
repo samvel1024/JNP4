87c49b623007f57125ece6c8f0c9fac8a36828eb
Jan Olkowski <jan.olkowski@gmail.com>
2018-12-03 22:56:16 +0100

    squares added

--- battle.h
+++ battle.h
@@ -8,30 +8,50 @@
 #include "math.h"
 
 
+template <int N>
+class Squares {
+private:
+	constexpr static int squares_table[N];
+
+public:
+	constexpr Squares() {
+		for (int i = 0; i < N; i++)	
+			squares_table[i] = i * i;
+	}
+	constexpr int get(int i) {
+		return squares_table[i];
+	}
+};	
+
 template<typename T, T t0, T t1, typename ... S>
 class SpaceBattle {
 private:
-    static_assert(0 <= t0 && t0 < t1, "Start time has to be less than end time");
+	static_assert(0 <= t0 && t0 < t1, "Start time has to be less than end time");
     const T start = t0;
     const T end = t1;
     T current = start;
+
+	constexpr static int square_number = 0;//TODO get_ceil(t1);
+	constexpr static Squares<square_number> squares = Squares<square_number>();
 
     std::tuple<S...> ships;
 
     size_t countType(bool imperial) {
         size_t ans = 0;
-        auto counter = [&](auto &x) {
+        auto count = [&](auto &x) {
             if (x.getShield() > 0 && imperial == x.isImperial())
                 ans++;
         };
-        std::apply([&](auto &...x) { (..., counter(x)); }, ships);
+        std::apply([&](auto &...x) { (..., count(x)); }, ships);
         return ans;
     }
 
 
 public:
 
-    explicit SpaceBattle(S &... args) : ships(std::make_tuple(args...)) {}
+    explicit SpaceBattle(S &... args) : ships(std::make_tuple(args...)) {
+		
+	}
 
 
     size_t countImperialFleet() {
@@ -44,7 +64,7 @@ public:
 
 
     void tick(T timeStep) {
-        current += timeStep;
+        /*current += timeStep;
         if (current >= end) { // Handle overflow
             current = end - current;
         }
@@ -61,7 +81,7 @@ public:
                 }
             };
             std::apply([&](auto &...a) { (..., foreach(a)); }, ships);
-        }
+        }*/
 
     }
 
