2aa5d4a8d2d4b7d204846bd28ae4e9bd7626234b
Jan Olkowski <jan.olkowski@gmail.com>
2018-12-04 14:35:27 +0100

    new version

--- battle.h
+++ battle.h
@@ -11,17 +11,17 @@
 template <int N>
 class Squares {
 private:
-	constexpr static int squares_table[N];
+	int squares_table[N];
 
 public:
 	constexpr Squares() {
-		for (int i = 0; i < N; i++)
+		for (int i = 0; i < N; i++)	
 			squares_table[i] = i * i;
 	}
-	constexpr int get(int i) {
+	constexpr int get(int i) const {
 		return squares_table[i];
 	}
-};
+};	
 
 template<typename T, T t0, T t1, typename ... S>
 class SpaceBattle {
@@ -31,61 +31,78 @@ private:
     const T end = t1;
     T current = start;
 
-	constexpr static int square_number = 0;//TODO get_ceil(t1);
-	constexpr static Squares<square_number> squares = Squares<square_number>();
+	constexpr static int square_number = 10;//TODO get_ceil(t1);
+	constexpr static auto squares = Squares<square_number>();
 
     std::tuple<S...> ships;
 
-    size_t countType(bool imperial) {
-        size_t ans = 0;
-        auto count = [&](auto &x) {
-            if (x.getShield() > 0 && imperial == x.isImperial())
-                ans++;
-        };
-        std::apply([&](auto &...x) { (..., count(x)); }, ships);
-        return ans;
-    }
-
-
-public:
-
-    explicit SpaceBattle(S &... args) : ships(std::make_tuple(args...)) {
-
+	template<class F, class... Args>	
+	void for_each(F func, std::tuple <Args...>& _tuple) {
+		std::apply([&](auto &...x) { (..., func(x)); }, _tuple);
+	}
+	
+	template <bool imperial>
+	size_t countType() {
+		size_t ans = 0;
+		auto count = [&](auto &x) {
+			if(x.getShield() > 0 && imperial == x.isImperial()) 
+				ans++;
+		};
+		for_each(count, ships);
+		return ans;		
 	}
 
+	bool checkIfSquare(int n) {
+		int begin = 0, end = square_number, middle;
+
+		while (begin < end) {
+			middle = (begin + end) / 2;
+			if( squares.get(middle) < n )
+				begin = middle + 1;
+			else
+				end = middle;	
+		}
+
+		if(squares.get(begin) == n)
+			return true;
+		else
+			return false;
+	}
+
+public:
+    explicit SpaceBattle(S &... args) : ships(std::make_tuple(args...)) {
+	
+	}
 
     size_t countImperialFleet() {
-        return countType(true);
+        return countType<true>();
     }
 
     size_t countRebelFleet() {
-        return countType(false);
+        return countType<false>();
     }
 
-
     void tick(T timeStep) {
-
-
-        if (current == 1 || current == 2 || current == 4 || current == 9 || current == 16 ) {
-            auto foreach = [&](auto &x) {
-                if (x.getShield() > 0 && x.isImperial()) {
-                    auto nested = [&](auto &y) {
-                        if (y.getShield() > 0 && !y.isImperial()) {
-
-                            attack(x, y);
-                        }
-                    };
-                    std::apply([&](auto &...b) { (..., nested(b)); }, ships);
-                }
-            };
-            std::apply([&](auto &...a) { (..., foreach(a)); }, ships);
-        }
-
-        current += timeStep;
-        if (current >= end) {
+		if( countImperialFleet() == 0 && countRebelFleet() == 0)
+			std::cout << "DRAW\n";
+		else if (countImperialFleet() == 0)
+			std::cout << "REBELLION WON\n";
+		else if (countRebelFleet() == 0)
+			std::cout << "IMPERIUM WON\n";
+		else if (checkIfSquare(current)) {			
+			for_each([&](auto &x) {
+					for_each([&](auto &y) {
+						if(x.getShield() > 0 && y.getShield() > 0) {
+							attack(x, y);
+						}
+					}, ships);
+			}, ships);            
+		}
+     
+		current += timeStep;
+        if (current >= end) { // Handle overflow
             current = end - current;
         }
-
     }
 
 };
